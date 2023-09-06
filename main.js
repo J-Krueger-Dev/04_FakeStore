b56af40a3a5dd8f748ad37c0523f4aa02e6d7715
J-Krueger-Dev <Jkrueger212@gmail.com>
2023-09-05 19:34:54 -0600

    changed some varibles around and event listeners created, ticket 3 complete'

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-let navbarCatergories= document.querySelector('.navbar-nav');
+let navbarElectronics =document.getElementById('electronics');
+let navbarJewelery =document.getElementById('jewelery');
+let navbarMen =document.getElementById('men')
+let navbarWomen =document.getElementById('women')
 let displayDiv=document.getElementById('display');
-let fakeStoreAPI='https://fakestoreapi.com/products/';
+let fakeStoreAPI='https://fakestoreapi.com/products/category/';
 
 const fakeStore = async (endpoint) =>{
   let result = await fetch(fakeStoreAPI + endpoint);
   let storeData = await result.json();
   console.log(storeData);
 }
+/* fakeStore('jewelery')
+fakeStore('electronics')
+fakeStore(`men's clothing`)
+fakeStore(`women's clothing`) */
+
+navbarElectronics.addEventListener('click', () =>{
+  fakeStore('electronics')
+})
+navbarJewelery.addEventListener('click', () =>{
+  fakeStore('jewelery')
+})
+navbarMen.addEventListener('click', () =>{
+  fakeStore(`men's clothing`)
+})
+navbarWomen.addEventListener('click', () =>{
+  fakeStore(`women's clothing`)
+})
+
+
 window.onload = (event) =>{
-  fakeStore('')
+  fakeStore('jewelery')
+  fakeStore('electronics')
+  fakeStore(`men's clothing`)
+  fakeStore(`women's clothing`)
 };
 

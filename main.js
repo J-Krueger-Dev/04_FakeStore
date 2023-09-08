d16cb77a7998f2a57b2461730784e1f9b45becd3
J-Krueger-Dev <Jkrueger212@gmail.com>
2023-09-07 20:08:38 -0600

    ticket 04 - added card and accordian, figuring out how to display all data.

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
 let navbarElectronics =document.getElementById('electronics');
 let navbarJewelery =document.getElementById('jewelery');
-let navbarMen =document.getElementById('men')
-let navbarWomen =document.getElementById('women')
+let navbarMen =document.getElementById('men');
+let navbarWomen =document.getElementById('women');
 let displayDiv=document.getElementById('display');
+let data=[]
 let fakeStoreAPI='https://fakestoreapi.com/products/category/';
+
+
 
 const fakeStore = async (endpoint) =>{
   let result = await fetch(fakeStoreAPI + endpoint);
   let storeData = await result.json();
   console.log(storeData);
-  displayCards(storeData)
+  displayCards(storeData);
 }
 
-function displayCards(){
-  // CREATE NEW ELEMENTS
+function displayCards(storeData){
+ console.log(storeData);
+ // CREATE NEW ELEMENTS
 /* 
   1. crete the new empty element
   2. edit that element
-    -adding text
-    -changing styling
+  -adding text
+  -changing styling
   3. add the element to the web page
-*/
+  */
+ let productPic=document.getElementById('picture');
+ let productTitle=document.getElementById('cardTitle');
+let productDesc=document.getElementById('desc');
+let productPrice=document.getElementById('price');
+//! for each
+productPic.alt=storeData.title;
+productPic.src=storeData.image;
+productTitle.innerText= storeData.title;
+productDesc.innertext=storeData.despcription;
+productPrice.innerText=storeData.price;
+
+
 }
 
 navbarElectronics.addEventListener('click', () =>{
